b0311216fe9f038f8f848f911c4e061a5c6b3463
Ziga Zajc <ziga.zajc007@gmail.com>
2019-10-22 17:30:38 +0200

    First commit

---
+++ src/main/java/com/rabbitcompany/admingui/AdminGUI.java
@@ -0,0 +1,152 @@
+package com.rabbitcompany.admingui;
+
+import com.rabbitcompany.admingui.commands.Admin;
+import com.rabbitcompany.admingui.listeners.InventoryClickListener;
+import com.rabbitcompany.admingui.listeners.PlayerDamageListener;
+import com.rabbitcompany.admingui.listeners.PlayerJoinListener;
+import com.rabbitcompany.admingui.listeners.PlayerLoginListener;
+import com.rabbitcompany.admingui.utils.Message;
+import com.rabbitcompany.admingui.utils.Updater;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
+public class AdminGUI extends JavaPlugin {
+
+    private static AdminGUI instance;
+
+    //VaultAPI
+    private static Economy econ = null;
+    public static boolean vault = false;
+
+    //Language
+    private File l = null;
+    private YamlConfiguration lang = new YamlConfiguration();
+
+    //Plugins
+    private File p = null;
+    private YamlConfiguration plug = new YamlConfiguration();
+
+    public static SpigotUpdater updater;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        this.l = new File(getDataFolder(), "language.yml");
+        this.p = new File(getDataFolder(), "plugins.yml");
+        mkdir();
+        loadYamls();
+
+        Bukkit.getConsoleSender().sendMessage(Message.chat("&7[&cAdmin GUI&7] &aPlugin is enabled!"));
+
+        //bStats
+        if(!Bukkit.getVersion().contains("1.8")){
+            MetricsLite metricsLite = new MetricsLite(this);
+        }
+
+        //Updater
+        //updater = new SpigotUpdater(this, 71157);
+
+        //VaultAPI
+        if(setupEconomy()){
+            vault = true;
+        }
+
+        //Check for updates
+        Updater.sendConsole();
+
+        //Listeners
+        new InventoryClickListener(this);
+        new PlayerDamageListener(this);
+        new PlayerJoinListener(this);
+        new PlayerLoginListener(this);
+
+        //Commands
+        this.getCommand("admin").setExecutor((CommandExecutor) new Admin());
+        this.getCommand("admin").setTabCompleter(new TabCompletion());
+    }
+
+    @Override
+    public void onDisable() {
+        Bukkit.getConsoleSender().sendMessage(Message.chat("&7[&cAdmin GUI&7] &4Plugin is disabled!"));
+    }
+
+    //VaultAPI
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return true;
+    }
+
+    public static Economy getEconomy() {
+        return econ;
+    }
+
+    private void mkdir(){
+        if (!this.l.exists()) {
+            saveResource("language.yml", false);
+        }
+
+        if(!this.p.exists()){
+            saveResource("plugins.yml", false);
+        }
+    }
+
+    public void loadYamls(){
+        try {
+            this.lang.load(this.l);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+        try {
+            this.plug.load(this.p);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public YamlConfiguration getLang() { return this.lang; }
+
+    public YamlConfiguration getPlug() { return this.plug; }
+
+    public void saveLang() {
+        try {
+            this.lang.save(this.l);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void savePlug(){
+        try {
+            this.plug.save(this.p);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static AdminGUI getInstance(){
+        return instance;
+    }
+}
---
+++ src/main/java/com/rabbitcompany/admingui/TabCompletion.java
@@ -0,0 +1,34 @@
+package com.rabbitcompany.admingui;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TabCompletion implements TabCompleter {
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+
+        if(command.getName().equalsIgnoreCase("admin")){
+            List<String> completions = new ArrayList<>();
+            if(args.length == 1){
+
+                for(Player all : Bukkit.getServer().getOnlinePlayers()) {
+                    completions.add(all.getName());
+                }
+
+                if(sender.hasPermission("admingui.reload")){
+                    completions.add("reload");
+                }
+            }
+            return completions;
+        }
+
+        return null;
+    }
+}
