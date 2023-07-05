3a50b4d4f2f97a8737fc8803e9fe0ea888dae62c
Ziga <ziga.zajc007@gmail.com>
2023-07-05 22:35:53 +0200

    Added website under support

--- src/main/java/com/rabbitcompany/admingui/AdminGUI.java
+++ src/main/java/com/rabbitcompany/admingui/AdminGUI.java
@@ -413,6 +413,7 @@ public class AdminGUI extends JavaPlugin implements PluginMessageListener {
 		Bukkit.getConsoleSender().sendMessage(Message.chat("&6|"));
 		Bukkit.getConsoleSender().sendMessage(Message.chat("&6| &cSupport:"));
 		Bukkit.getConsoleSender().sendMessage(Message.chat("&6|"));
+		Bukkit.getConsoleSender().sendMessage(Message.chat("&6|   &9Website: &6https://rabbit-company.com"));
 		Bukkit.getConsoleSender().sendMessage(Message.chat("&6|   &9Discord: &bziga.zajc007"));
 		Bukkit.getConsoleSender().sendMessage(Message.chat("&6|   &9Mail: &bziga.zajc007@gmail.com"));
 		Bukkit.getConsoleSender().sendMessage(Message.chat("&6|   &9Discord: &bhttps://discord.gg/hUNymXX"));
